16a21739d7f0ca8a95677db0f046aaa9c2e60438
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2022-07-21 13:46:28 +0200

    tests

--- sep-import/test/run.sh
+++ sep-import/test/run.sh
@@ -11,18 +11,21 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
   rm -rf $LOG_DIR/*;
   FAILED=0
 
+  cd $scriptDir
+
 ############################################################################################################################
 # Run
 
   logFile="$LOG_DIR/$scriptName.out";
   mkdir -p "$(dirname "$logFile")";
 
-  runScript="npx sep-async-api-importer -fp ../../specs/**/*.spec.yml"
+  runScript="npx sep-async-api-importer -fp $scriptDir/../../specs/**/*.spec.yml"
   echo "starting: $runScript ..."
 
-  $runScript | npx pino-pretty
-  # $runScript | npx pino-pretty > $logFile 2>&1
-  code=$?;
+  $runScript 2>&1 | npx pino-pretty 2>&1 > $logFile 2>&1
+
+  code=${PIPESTATUS[0]}
+
   if [[ $code != 0 ]]; then
     echo ">>> ERROR - code=$code - runScript='$runScript' - $scriptName";
     echo ">>> See log file for details: $logFile"
@@ -34,6 +37,18 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 
 filePattern="$LOG_DIR"
 errors=$(grep -n -r -e " ERROR " $filePattern )
+cliErrors=$(grep -n -r -e " CliError " $filePattern )
+
+if [ ! -z "$cliErrors" ]; then
+  FAILED=1
+  echo "   found ${#cliErrors[@]} Cli Errors"
+  while IFS= read line; do
+    echo $line >> "$LOG_DIR/$scriptName.ERROR.out"
+  done < <(printf '%s\n' "$cliErrors")
+else
+  echo "   no CliError found"
+fi
+
 if [ ! -z "$errors" ]; then
   FAILED=1
   echo "   found ${#errors[@]} ERRORS"
